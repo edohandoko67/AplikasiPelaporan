e7b09b7f5c1bdf3a023a9a4e2fb84e1db7554247
edohandoko67 <edohandoko67@gmail.com>
2024-02-02 11:24:02 +0700

    notification done

--- app/src/main/java/com/example/aplikasipelaporan/login/LoginActivity.java
+++ app/src/main/java/com/example/aplikasipelaporan/login/LoginActivity.java
@@ -2,17 +2,24 @@ package com.example.aplikasipelaporan.login;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.NotificationCompat;
+import android.app.PendingIntent;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.aplikasipelaporan.R;
+import com.example.aplikasipelaporan.model.Report;
 import com.example.aplikasipelaporan.ui.DashboardActivity;
 import com.example.aplikasipelaporan.ui.DashboardUnitAct;
 import com.example.aplikasipelaporan.ui.DashboardUser;
@@ -80,6 +87,7 @@ public class LoginActivity extends AppCompatActivity {
                                         if (userRole.contains("admin")){
                                             sharedPref.setUserRole("admin");
                                             startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                                            notifFCM();
                                         } else if (userRole.contains("unit")){
                                             sharedPref.setUserRole("unit");
                                             startActivity(new Intent(LoginActivity.this, DashboardUnitAct.class));
@@ -110,6 +118,50 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }
             });
+        });
+    }
+
+    private void notifFCM() {
+        DatabaseReference reportsRef = FirebaseDatabase.getInstance().getReference("reports");
+        reportsRef.orderByChild("status").equalTo("OPEN").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Report report = snapshot.getValue(Report.class);
+
+                    if (report != null && !snapshot.hasChild("tanggalUpdate")) {
+                        sendNotification("Terdapat Notif Laporan", "Mohon segera cek di list notification");
+                    }
+                }
+            }
+
+            private void sendNotification(String title, String body) {
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                PendingIntent pendingIntent = PendingIntent.getActivity(LoginActivity.this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    NotificationChannel channel = new NotificationChannel("default_channel_id", "Default Channel", NotificationManager.IMPORTANCE_DEFAULT);
+                    channel.setDescription("Default Channel Description");
+                    channel.enableLights(true);
+                    channel.setLightColor(Color.BLUE);
+                    channel.enableVibration(true);
+                    notificationManager.createNotificationChannel(channel);
+                }
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(LoginActivity.this, "default_channel_id")
+                        .setAutoCancel(true)
+                        .setSmallIcon(R.drawable.baseline_message_24)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setContentIntent(pendingIntent);
+                notificationManager.notify(0, notificationBuilder.build());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("Firebase", "Error: " + error.getMessage());
+            }
         });
     }
 
